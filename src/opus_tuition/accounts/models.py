2ed635102f8452687cc25b46aa8708ca958dc94c
Tang Hui En <atanghuien@gmail.com>
2026-05-30 16:31:44 +0800

    feat: add models in accounts app

--- src/opus_tuition/accounts/models.py
+++ src/opus_tuition/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Tutor(models.Model):
+    tutor_id = models.CharField(max_length=100, primary_key=True)
+    tutor_name = models.CharField(max_length=100)
+    tutor_email = models.EmailField(unique=True)
+
+class Student(models.Model):
+    student_id = models.AutoField(primary_key=True)
+    student_name = models.CharField(max_length=100)
+
