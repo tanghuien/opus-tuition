5c35d058c0ca1fb8d82b001e5d8164dfd2f9930f
Tang Hui En <atanghuien@gmail.com>
2026-06-22 01:32:21 +0800

    refactor: remove primary key of Tutor and Student and update max length of tutor's email

--- src/opus_tuition/accounts/models.py
+++ src/opus_tuition/accounts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Tutor(models.Model):
     tutor_name = models.CharField(max_length=100)
-    tutor_email = models.EmailField(unique=True)
+    tutor_email = models.EmailField(max_length=254), unique=True)
 
 class Student(models.Model):
     student_name = models.CharField(max_length=100)
